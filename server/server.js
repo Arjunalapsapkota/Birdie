88eb23edcfece83072117439e0bd8d6f3974701b
arjunalapsapkota <arjunalapsapkota@gmail.com>
2019-03-12 10:33:12 -0700

    Heroku TRY05 Path-issue

--- server/server.js
+++ server/server.js
@@ -10,13 +10,14 @@ app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("/client/build"));
-} else app.use(app.use(express.static("/client/public")));
+}
 // Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
+
 app.get("*", (req, res) => {
-  res.sendFile("./client/build/index.html");
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
 
 app.listen(PORT, () => {
