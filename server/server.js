02211fed7b9dc249e19644f42d15e57ccb039210
arjunalapsapkota <arjunalapsapkota@gmail.com>
2019-03-24 10:25:06 -0700

    path-test-03

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build"));
+  app.use(express.static("client/build"));
 }
 // Define API routes here
 
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 // Define any API routes before this runs
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
