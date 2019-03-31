d59b0ac61030da879f001a2b214191f6d2bd11b5
arjunalapsapkota <arjunalapsapkota@gmail.com>
2019-03-31 16:12:44 -0700

    set to run on heroku

--- server/server.js
+++ server/server.js
@@ -11,15 +11,27 @@ const cookieSession = require("cookie-session");
 const passportSetup = require("./config/passport-setup");
 var cors = require("cors");
 // app.use(cors());
-app.use("*", function(req, res, next) {
-  //replace localhost:8080 to the ip address:port of your server
+// app.use("*", function(req, res, next) {
+//   //replace localhost:8080 to the ip address:port of your server
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next();
+// });
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  if (req.method === "OPTIONS") {
+    return res.status(200).send("ok");
+  } else {
+    next();
+  }
 });
-
 //enable pre-flight
 app.options("*", cors());
 
