b55577caade73e3d4cd8d70005adff58788177eb
arjunalapsapkota <arjunalapsapkota@gmail.com>
2019-11-13 06:57:38 -0800

    Route protection

---
+++ client/src/components/Photo/Photo.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Navbar from "../Navbar/Navbar.js";
+class Photo extends Component {
+  state = {};
+  render() {
+    return <Navbar></Navbar>;
+  }
+}
+
+export default Photo;
