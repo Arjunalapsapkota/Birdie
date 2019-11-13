06b06a026d5a86839f91b788e04f61889332710f
arjunalapsapkota <arjunalapsapkota@gmail.com>
2019-11-13 07:41:46 -0800

    Route protection

--- client/src/localStorage.js
+++ client/src/localStorage.js
@@ -1,6 +1,6 @@
 export const loadState = () => {
   try {
-    const serializedState = localStorage.getItem("state");
+    const serializedState = localStorage.getItem("$birdiez2019state$local");
     if (serializedState === null) {
       return undefined;
     }
@@ -12,7 +12,7 @@ export const loadState = () => {
 export const saveState = state => {
   try {
     const serializedState = JSON.stringify(state);
-    localStorage.setItem("state", serializedState);
+    localStorage.setItem("$birdiez2019state$local", serializedState);
   } catch (err) {
     //return undefined;
   }
